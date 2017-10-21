fdad752a08604aa563429565f0a2fbd4414faea2
ZirafIzirafla <alfarizi266@gmail.com>
2017-10-21 17:22:37 +0700

    Update and rename main.py to ohm.py

---
+++ ohm.py
@@ -0,0 +1,19 @@
+def volt(i,r):
+
+  print (i * r)
+
+def ohm(v,i):
+
+  print (v / i)
+
+def ampere(v,r):
+
+  print (v / r)
+  
+#This is a simple implementation of Ohm's rules
+
+volt(4,6)
+
+ohm(6,2)
+
+ampere(8,5)
